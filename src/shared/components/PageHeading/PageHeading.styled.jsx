53660e1e132881559442ed96373685d95712f636
vklya <glushenkovika@gmail.com>
2023-05-12 01:01:28 +0300

    add pages main heading

---
+++ src/shared/components/PageHeading/PageHeading.styled.jsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Heading = styled.h1`
+    text-align: center;
+    font-family: ${({ theme }) => theme.fonts.main.bold};
+    font-weight: 700;
+    font-size: ${({ theme }) => theme.fontSizes[4]};
+    line-height: 33px;  
+    color: ${({ theme }) => theme.colors.black};
+
+    @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+        font-size: ${({ theme }) => theme.fontSizes[6]};
+        line-height: 66px; 
+    }
+`
