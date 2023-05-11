40fd37d003b7193d05b94c2b8ca7c4a403ba8acb
Maryna Skoryk <skoryk.maryna@gmail.com>
2023-05-11 13:16:13 +0200

    delete theme

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-// import { theme } from './theme/theme';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/your-pet">
     <React.StrictMode>
-      {/* <ThemeProvider theme={theme}> */}
       <App />
-      {/* </ThemeProvider> */}
     </React.StrictMode>
   </BrowserRouter>
 );
