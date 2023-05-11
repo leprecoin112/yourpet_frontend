e0ba87369166dbbc662daa6d2344e969cb26b6dc
vklya <glushenkovika@gmail.com>
2023-05-12 00:32:53 +0300

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from '@emotion/react';
 import lightTheme from './shared/styled/theme/lightTheme';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={lightTheme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
