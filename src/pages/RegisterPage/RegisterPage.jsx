7ca570da13bbe4bb11ceed4b2b451a0b9cb1fc64
Zhanna55 <zhannafesyk@gmail.com>
2023-05-12 09:45:20 +0300

    add page

---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,13 @@
+import Section from '../../shared/components/Section/Section';
+import Container from '../../shared/components/Container/Container';
+// import AuthForm from '../../shared/components/'
+
+const RegisterPage = () => {
+  return (
+    <>
+      <Section>
+        <Container>{/* <AuthForm/> */}</Container>
+      </Section>
+    </>
+  );
+};
--- src/shared/components/Section/Section.styled.jsx
+++ src/shared/components/Section/Section.styled.jsx
@@ -1,10 +1,11 @@
 import styled from '@emotion/styled';
-
+import 
 const Wrapper = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100vw;
+  background-image:url(${})
 `;
 
 export { Wrapper };
