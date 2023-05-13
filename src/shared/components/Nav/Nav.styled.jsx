e1a22d1f6a5544bd147e154f7bb2e3bb8c356124
YevhenSerdiuk2022 <jcserdyuk@gmail.com>
2023-05-13 12:49:41 +0300

    fix header

--- src/shared/components/Nav/Nav.styled.jsx
+++ src/shared/components/Nav/Nav.styled.jsx
@@ -10,13 +10,17 @@ color: ${({ theme }) => theme.colors.black};
 text-decoration: none;
 font-size: 20px;
 line-height: 1.35;
+
 font-family: ${({ theme }) => theme.fonts.main.regular};
 &.active {
+    color: ${({ theme }) => theme.colors.yellow};
+}
+&:hover {
     color: ${({ theme }) => theme.colors.yellow};
 }
 `; 
 export const NavEl = styled.nav`
 display: flex;
 align-items: center;
-gap: 12px;
+gap: 40px;
 `;
