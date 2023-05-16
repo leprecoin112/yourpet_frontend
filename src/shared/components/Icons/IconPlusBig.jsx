f437702b50fa7755d728099d85d5c0f52114a77a
vklya <glushenkovika@gmail.com>
2023-05-16 17:50:29 +0300

    fix icons and header

--- src/shared/components/Icons/IconPlusBig.jsx
+++ src/shared/components/Icons/IconPlusBig.jsx
@@ -1,8 +1,8 @@
 //change *stroke* to change color
 
-const IconPlusBig = () => {
+const IconPlusBig = ({className}) => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width='24' height='24' fill='none'>
+        <svg xmlns="http://www.w3.org/2000/svg" width='24' height='24' fill='none' className={className}>
             <path d="M12 20v-8m0 0V4m0 8h8m-8 0H4" strokeWidth="2" strokeLinecap="round"/>
         </svg>
     );
