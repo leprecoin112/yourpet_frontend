6d7cb5ffc448d36f9b24b4aa8cd065a757549d28
Yaroslav Khakhuda <hahyda14@gmail.com>
2023-05-20 20:22:24 +0300

    fix register form and login

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,16 +1,18 @@
 import AuthForm from '../../shared/components/AuthForm';
-import { useDispatch, useSelector, } from 'react-redux';
-import {isAuth} from '../../shared/redux/auth/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { isAuth } from '../../shared/redux/auth/selectors';
 import { Navigate } from 'react-router-dom';
-import { FormWrapper, FormTitle, RedirectLink, Link } from './LoginPage.styled';
+import { FormWrapper } from './LoginPage.styled';
 import Section from '../../shared/components/Section/Section';
 import Container from '../../shared/components/Container/Container';
+import RedirectLink from '../../shared/components/RedirectLink/RedirectLink';
+import FormTitle from '../../shared/components/FormTitle/FormTitle';
 
 const LoginPage = () => {
   const isLogin = useSelector(isAuth);
 
-  if(isLogin){
-    return <Navigate to="/user"/>
+  if (isLogin) {
+    return <Navigate to="/user" />;
   }
   return (
     <Section>
