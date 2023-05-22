7a4020584874a236c9cf8fc0cb7afe1dacb2d44a
Yaroslav Khakhuda <hahyda14@gmail.com>
2023-05-22 22:36:26 +0300

    add loader ourfriends

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -1,27 +1,22 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import OurFriendsList from '../../module/OurFriends/OurFriendsList/OurFriendsList';
 import Container from '../../shared/components/Container/Container';
 import { Title } from './OurFriendsPage.styled';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchFriends } from '../../shared/redux/friends/operations';
-import { getAllFriends, loading } from '../../shared/redux/friends/selectors';
 import Loader from '../../shared/components/Loader';
+import { useAllSponsorsQuery } from '../../shared/redux/api/backend/sponsors/sponsorsApi';
+import Section from '../../shared/components/Section/Section';
 
 const OurFriendsPage = () => {
-  const dispatch = useDispatch();
-  const ourFriendsList = useSelector(getAllFriends);
-  const isLoading = useSelector(loading);
-
-  useEffect(() => {
-    dispatch(fetchFriends());
-  }, [dispatch]);
-
+  const { data, isFetching, isLoading } = useAllSponsorsQuery();
+  const isLoader = isLoading || isFetching;
   return (
-    <Container>
-      <Title>Our Friends</Title>
-      {isLoading && <Loader />}
-      <OurFriendsList data={ourFriendsList} />
-    </Container>
+    <Section>
+      <Container>
+        <Title>Our Friends</Title>
+        {isLoader && <Loader />}
+        <OurFriendsList data={data?.result} />
+      </Container>
+    </Section>
   );
 };
 
