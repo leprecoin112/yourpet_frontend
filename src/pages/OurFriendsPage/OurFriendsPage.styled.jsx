613108a8da8117793dbcd798e4ea2859079316da
Yaroslav Khakhuda <hahyda14@gmail.com>
2023-05-22 23:34:14 +0300

    add padding bottom section

--- src/pages/OurFriendsPage/OurFriendsPage.styled.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.styled.jsx
@@ -1,18 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Title = styled.h1`
-padding-top: 45px;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 1.375;
+  text-align: center;
 
-font-weight: 700;
-font-size: 24px;
-line-height: 1.375;
-text-align: center;
-
-@media (min-width: 768px) {
-  padding-top: 90px;
-  font-size: 48px;
-}
-@media (min-width: 1280px) {
-  padding-top: 70px;
-}
+  @media (min-width: 768px) {
+    font-size: 48px;
+  }
 `;
--- src/shared/components/Section/Section.styled.jsx
+++ src/shared/components/Section/Section.styled.jsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
 const Wrapper = styled.section`
   padding-top: 40px;
+  padding-bottom: 40px;
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
     padding-top: 80px;
+    padding-bottom: 80px;
   }
 `;
 
